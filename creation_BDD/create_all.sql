904e83175f50c56fa331440b9d17dfe450224769
Fabien-B <fabien.bonneval@gmail.com>
2015-04-27 01:32:16 +0200

    [BDD] ajout d'un script pour creer rapidement la BDD.

---
+++ creation_BDD/create_all.sql
@@ -0,0 +1,9 @@
+drop database if exists test_asa;
+create database test_asa;
+use test_asa;
+source Base.SQL;
+source Compteurs.sql;
+source exploitants.sql;
+source parcelles.sql;
+source propriete.sql;
+source releves.sql
