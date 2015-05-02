59fe2ba3003d443bbcaa41b4788bdde42ef941f4
Fabien-B <fabien.bonneval@gmail.com>
2015-05-02 16:13:55 +0200

    [Haut de page] ajoute la gestion des pages

--- www/template.py
+++ www/template.py
@@ -1,61 +1,57 @@
-rep = 'exempleWebPython/'  # sous-repertoire d'installation de cet exemple dans www
+rep = 'asa/'  # sous-repertoire d'installation de cet exemple dans www
 
+def afficherHautPage(error = '', titre=''):
+    ret = '''
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <title>Test WEB python sous Karrigell</title>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <meta name="language" content="FR"/>
+            <meta name="viewport" content="initial-scale=1.0,width=device-width,user-scalable=yes" />
+            <meta name="description" content="Test python web" />
+            <link rel="stylesheet" href="../stylesheets/base.css">
+            <link rel="stylesheet" href="../stylesheets/skeleton.css">
+            <link rel="stylesheet" href="../stylesheets/layout.css">
+            <link rel="stylesheet" href="../stylesheets/flexslider.css">
+            <link rel="stylesheet" href="../stylesheets/prettyPhoto.css">
+            <link rel="stylesheet" href="../stylesheets/perso.css">
+            <script type="text/javascript" src="/'''+rep+'''js/jquery-2.1.3.js"></script>
+            <script type="text/javascript" src="/'''+rep+'''js/monSite.js"></script>
+        </head>
+        <body>
+            <header id="header" class="site-header" role="banner">
+                <div id="header-inner" class="container sixteen columns over">
+                    <hgroup class="four columns alpha">
+                        <h1 id="site-title" class="site-title">
+                            <a href="index" id="logo"><img src="images/icebrrrg-logo.png" alt="Icebrrrg logo" height="63" width="157" /></a>
+                        </h1>
+                    </hgroup>
+                    <nav id="main-nav" class="eight columns alpha">
+                        <ul id="main-nav-menu" class="nav-menu">'''
 
-def afficherHautPage(error = ''):
-    return '''
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>Test WEB python sous Karrigell</title>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="language" content="FR"/>
-        <meta name="viewport" content="initial-scale=1.0,width=device-width,user-scalable=yes" />
-        <meta name="description" content="Test python web" />
-        <link rel="stylesheet" href="../stylesheets/base.css">
-        <link rel="stylesheet" href="../stylesheets/skeleton.css">
-        <link rel="stylesheet" href="../stylesheets/layout.css">
-        <link rel="stylesheet" href="../stylesheets/flexslider.css">
-        <link rel="stylesheet" href="../stylesheets/prettyPhoto.css">
-        <link rel="stylesheet" href="../stylesheets/perso.css">
-        <script type="text/javascript" src="/'''+rep+'''js/jquery-2.1.3.js"></script>
-        <script type="text/javascript" src="/'''+rep+'''js/monSite.js"></script>
-    </head>
-    <body>
-        <header id="header" class="site-header" role="banner">
-            <div id="header-inner" class="container sixteen columns over">
-                <hgroup class="four columns alpha">
-                    <h1 id="site-title" class="site-title">
-                        <a href="index" id="logo"><img src="images/icebrrrg-logo.png" alt="Icebrrrg logo" height="63" width="157" /></a>
-                    </h1>
-                </hgroup>
-                <nav id="main-nav" class="eight columns alpha">
-                    <ul id="main-nav-menu" class="nav-menu">
-                        <li id="menu-item-1" class="current">
-                            <a href="index.html">Accueil</a>
-                        </li>
-                        <li id="menu-item-2">
-                            <a href="three-column.html">Ma Conso</a>
-                        </li>
-                        <li id="menu-item-3">
-                            <a href="sidebar-right.html">Relevés</a>
-                        </li>
-                        <li id="menu-item-4">
-                            <a href="sidebar-left.html">Évènements</a>
-                        </li>
-                        <li id="menu-item-5">
-                            <a href="full-width.html">Le Réseau</a>
-                        </li>
-                        <li id="menu-item-6">
-                        </li>
-                    </ul>
+    ret += create_link('menu-item-1', 'Accueil', titre,'index.py')
+    ret += create_link('menu-item-2', 'Ma Conso', titre,'page_conso.py')
+    ret += create_link('menu-item-3', 'Relevés', titre,'page_releves.py')
+    ret += create_link('menu-item-4', 'Évènements', titre,'page_evenements.py')
+    ret += create_link('menu-item-5', 'Le Réseau', titre,'page_reseau.py')
+
+    ret += '''          </ul>
                 </nav>
                 <div id="login" class="four columns omega">
                 '''+afficherFormulaireConnexion(error)+'''
                 </div>
             </div>
-        </header>
+        </header>'''
+    return ret
 
-    '''
+def create_link(position, titre, current_page,fichier):
+    current = ' class="current"' if titre == current_page else ''
+    return '''
+    <li id="{0}"{1}>
+        <a href="../{2}">{3}</a>
+    </li>'''.format(position,current,fichier, titre)
+
 
 
 def afficherFormulaireConnexion(error = ''):
