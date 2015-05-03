c89dd95bf108fdbdc566483468b9bdf3b2d592d9
Fabien-B <fabien.bonneval@gmail.com>
2015-05-03 21:44:37 +0200

    [Sessions] controle du login et du mot de passe à la connexion. Les logins vont de 'login0' à 'login81' avec le mot de passe 'azerty'

--- www/gestion_session.py
+++ www/gestion_session.py
@@ -1,10 +1,27 @@
+import mysql.connector
+import bcrypt
 
 def Connexion(page, choix, login='',password=''):
     if choix=='deconnecter':
         if "login" in Session(): del Session()["login"]
         return page('Deconnecte')
     else:
-        if password != 'test':
+        connection = mysql.connector.connect(user='root', password='root',host='127.0.0.1',database='asa')
+        curseur = connection.cursor()
+        requete = 'select login from Exploitant;'
+        curseur.execute(requete)
+        logins_tuples=curseur.fetchall()
+        logins_list = []
+        for (log,) in logins_tuples:
+            logins_list.append(log)
+        if not login in logins_list:
+            return page('error')
+        #else:
+        requete = "select Password,Salt from Exploitant where login='{}';".format(login)
+        curseur.execute(requete)
+        (pw_base,salt)=curseur.fetchall()[0]
+        pw_user = bcrypt.hashpw(password,salt)
+        if pw_user == pw_base:
             Session()["login"] = login;
             return  page()
         else:
