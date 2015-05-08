3e599da200f229c3ea6471accdfad6f0fc3168ea
Fabien-B <fabien.bonneval@gmail.com>
2015-05-08 13:31:36 +0200

    [Conso] start conso page.

--- www/page_conso.py
+++ www/page_conso.py
@@ -3,15 +3,45 @@ connexion = Import('gestion_session.py')
 
 def index(error=''):
     ret=template.afficherHautPage(error, titre='Ma Conso')
-    ret += corps_page()
+    if "login" in Session():
+        ret += corps_page_connecte()
+    else:
+        ret += corps_page_deconnecte()
     ret += template.afficherBasPage()
     return ret
 
-def corps_page():
+def corps_page_connecte():
     html = """
-    <p>Je bois de l'eauuuuuu!!!</p>
+    <p>Bonjour!</p>
+    <table class="table_conso">
+    <caption>Votre consommation</caption>
+    <tr>
+        <th>Compteur</th>
+        <th>Parcelle</th>
+        <th>Consommation</th>
+    </tr>"""
+    html += add_line('aze','bvcc','456')
+    html += """
+</table>
     """
     return html
 
+def corps_page_deconnecte():
+    html = """
+    <p>Bonjour! Veuillez vous connecter.</p>
+    """
+    return html
+
+def add_line(compteur='', parcelle='', conso=None):
+    line = """
+    <tr>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+    </tr>""".format(compteur,parcelle,conso)
+    return line
+
 def traiterFormulaireConnexion(choix, login='',password=''):
     return connexion.Connexion(index, choix, login, password)
+
+# '''select Releve.Id_releve, Releve.Compteur, Releve.Exploitant, Releve.Index_début, Releve.Index_fin,Releve.Date FROM Releve WHERE Exploitant = 1 ORDER BY Compteur,Date;'''
