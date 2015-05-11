f5fde23d5885e96dc0ed4b1c6a54e8a68e34998c
didzoskill <nicolas.dzieciol@gmail.com>
2015-05-11 14:00:36 +0200

    [TEMPLATE] Remodif qui était partie

--- www/template.py
+++ www/template.py
@@ -37,7 +37,7 @@ def afficherHautPage(error = '', titre=''):
                 '''+afficherFormulaireConnexion(error)+'''
                 </div>
             </div>
-            <div id="header-inner" class="container sixteen columns over">
+            <div id="header-inner" class="container ten columns alpha">
                     <nav id="main-nav">
                         <ul id="main-nav-menu" class="nav-menu">'''
     ret += create_link('menu-item-1', 'Accueil', titre,'index.py')
