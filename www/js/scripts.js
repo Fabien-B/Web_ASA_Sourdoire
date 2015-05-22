75729aa2d0b1d268ad052e79ac5b3f1a9776ed67
Fabien-B <fabien.bonneval@gmail.com>
2015-05-22 02:45:08 +0200

    [releves] javascript function to update text input on combo box change.

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -61,4 +61,15 @@ function update_conso() {
 	});	   
 }
 
+function update_index_deb_releve(id_compteur) {
+   $.ajax({
+		type:"get",
+		url:'../page_releves.py/part_index_debut',   // fonction python appelée
+		data: {'id_compteur':id_compteur}, // parametres passes a cette fonction
+		success:function(reponse){  // recup dans reponse du return fait par la fonction corps_page_connecte
+			$("#index_debut_releves").html(reponse);   // maj sur la page 
+		},
+		error:function(){ alert("erreur lors de la recuperation de la page");}
+	}); 
+}
 //End document.ready
