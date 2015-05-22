f485d7dfe63a48498e6868d235236ef43bf82dc7
Fabien-B <fabien.bonneval@gmail.com>
2015-05-22 04:02:24 +0200

    [network] fill inspector.

--- www/page_reseau.py
+++ www/page_reseau.py
@@ -2,6 +2,7 @@ template = Import('template.py' ) # import du fichier template (entete, pieds de
 connexion = Import('gestion_session.py')
 compteur = Import('compteur.py')
 import json
+import os
 
 def index(error=''):
     ret=template.afficherHautPage(error, titre='Voir le réseau')
@@ -10,25 +11,43 @@ def index(error=''):
     return ret
 
 def corps_page():
-    html = """
+    html = '''
     <div class = container>
         <div class="sixteen columns over" style="text-align: center;">
             <h1>Le réseau d'irrigation</h1>
         </div>
-    	<aside class="six columns main-content">
-	        <h2>Compteur séléctionné</h2>
-	        <span>
-	            <img src="../images/pompe.png" height=100px style="border:2px; border-color:#004A4D; border-style:groove; padding:5px;"></img>
-	            <h3>Nom :</h3>
-	        </span>
-	    </aside>
+        <aside class="six columns main-content">
+            <h2>Compteur séléctionné</h2>
+    '''
+    html += detail(0)
+    html += '''</aside>
         <article class="ten columns right-sidebar">
             <div id="map" style="height: 700px"></div>
             <script type="text/javascript" src="../js/map.js"></script>
-	        <script type="text/javascript" src="../js/map_network.js"></script>
-	    </article>
-	</div>
-    """
+            <script type="text/javascript" src="../js/map_network.js"></script>
+        </article>
+    </div>
+    '''
+    return html
+
+def detail(id_compteur=1):
+    id_compteur = int(id_compteur)
+    selected_comp = compteur.Compteur(id_compteur)
+
+    path = '../images/compteur_{0}.png'.format(id_compteur)
+    if not os.path.exists(path):
+        path = '../images/pompe.png'
+
+    nom = selected_comp.nom if selected_comp.nom else 'Donnée manquante'
+    latitude = selected_comp.lat if selected_comp.lat else 'Donnée manquante'
+    longitude = selected_comp.lon if selected_comp.lon else'Donnée manquante'
+    altitude = str(selected_comp.altitude) + ' m' if selected_comp.altitude else'Donnée manquante'
+    html = '''<span id="detail_compteur">
+                <img src="{0}" height=100px style="border:2px; border-color:#004A4D; border-style:groove; padding:5px;"></img>
+                 <h3>Nom : {1}</h3>
+                 <h3>position : {2}, {3}</h3>
+                 <h3>Altitude : {4}</h3>
+            </span>'''.format(path,nom,latitude,longitude,altitude)
     return html
 
 def get_json_compteurs(ex=not None):
@@ -49,7 +68,7 @@ def compteurs_to_json(list_compteurs):
         if current_com.lon != None and current_com.lat != None:
             dico_current = {}
             dico_current["type"] = "Feature"
-            dico_current["properties"] = {"id":current_com.id,"altitude": altitude}
+            dico_current["properties"] = {"id":current_com.id,"nom":current_com.nom,"altitude": altitude}
             dico_current["geometry"]= {"type": "Point", "coordinates": [current_com.lon, current_com.lat]}
             list_json.append(dico_current)
     dico["features"] = list_json
