af66aecad2e9212462fc84eff483742618b81a6b
didzoskill <nicolas.dzieciol@gmail.com>
2015-05-23 22:07:40 +0200

    [Page event] Maj de la page, ce n'est pas encore effectif

--- www/page_evenements.py
+++ www/page_evenements.py
@@ -5,6 +5,7 @@ parcelle = Import('parcelle.py')
 template = Import('template.py' ) # import du fichier template (entete, pieds de page...)
 connexion = Import('gestion_session.py')
 
+
 def index(error=''):
     ret=template.afficherHautPage(error, titre='Signaler un évènement')
     if "login" in Session():
@@ -23,43 +24,71 @@ def corps_page_deconnecte():
 
 
 def corps_page_connecte():
-    options = recup_options()
+    options = get_combo_box(0)
     html = '''
     <div class="container">
     <div class="container" ><h1 style="text-align: center;margin-bottom:15px;">Signaler un évènement</h1></div>
-    <form method="post" action="reception.php" enctype="multipart/form-data">
-    <div id="choose_compteur" style="margin-bottom:25px;">
-        <h3>
-            L'évènement concerne:
-        </h3>
-        <div style="display:inline-flex; width:100%;text-align:center;font-weight: bold;" >
-            <form>
-                <div class="alpha" style="float:left;width:50%;text-decoration:bold;"><INPUT type="checkbox" name="choix1" value="1">Un de vos compteur / parcelles </div>
-                <div class="alpha" style="float:right;width:50%;"><INPUT type="checkbox" name="choix2" value="2"> Un autre compteur / parcelle </div>
-            </form>
-        </div>
+    <form method="post" action="reception">
+        <div id="choose_compteur" style="margin-bottom:25px;">
+            <h3>
+                L'évènement concerne:
+            </h3>
+            <div style="display:inline-flex; width:100%;text-align:center;font-weight: bold;" >
+                <form>
+                    <div class="alpha" style="float:left;width:50%;text-decoration:bold;"><INPUT TYPE="radio" NAME= "choix" VALUE="0" CHECKED onclick="change_combo_box(this.value)">Un de vos compteur / parcelles </div>
+                    <div class="alpha" style="float:right;width:50%;"><INPUT TYPE="radio" NAME= "choix" VALUE="1" onclick="change_combo_box(this.value)">Un autre compteur / parcelle </div>
+                </form>
+            </div>
+        </div>'''
+
+    html+=get_combo_box(0)
+
+    html+='''
+        <label for="description">Description de l'évènement :</label>
+
+        <textarea name="description" id="description"></textarea>
+
+        <label for="mon_fichier">Photo (tous formats | max. 5 Mo) :</label>
+
+        <input type="hidden" name="MAX_FILE_SIZE" value="5242880" />
+
+        <input type="file" name="mon_fichier" id="mon_fichier" />
+
+        <br /><br />
+
+        <input type="submit" name="submit" value="Envoyer" />
+    </form>
     </div>
-    <p> Selectionner le compteur concerné :<select id="combo_compteur_evenement" name="id_compteur"> </p>
-        {0}
-        </select></p>
-    <label for="description">Description de l'évènement (max. 255 caractères) :</label>
-     <textarea name="description" id="description"></textarea>
-     <label for="mon_fichier">Photo (tous formats | max. 5 Mo) :</label>
-     <input type="hidden" name="MAX_FILE_SIZE" value="5242880" />
-     <input type="file" name="mon_fichier" id="mon_fichier" />
-     <br />
-     <input type="submit" name="submit" value="Envoyer" />
-</form>
-    </div>
-    '''.format(options)
+    '''
     return html
 
-def recup_options():
+
+def recup_options(id_ex):
     options = ''
-    compteurs_parc_id = compteur.Compteur.get_compteurs_parcelle_id(0)
+    compteurs_parc_id = compteur.Compteur.get_compteurs_parcelle_id(id_ex)
     for (id_compt, id_parc) in compteurs_parc_id:
         current_parc = parcelle.Parcelle(id_parc)
         current_compt = compteur.Compteur(id_compt)
         line = '<option value="{}"> '.format(id_compt) + str(id_compt) + ', ' + current_compt.nom + ': ' + current_parc.nom.capitalize() + ' </option>' + '\n'
         options += line
     return options
+
+
+def traiterFormulaireConnexion(choix, login='',password=''):
+    return connexion.Connexion(index, choix, login, password)
+
+
+def reception():
+    pass
+
+def get_combo_box(opt_check = None):
+    if not opt_check:
+        options = recup_options(Session()["Id_exploitant"])
+    else:
+        options = recup_options(0)
+
+    html='''
+    <p> Selectionner le compteur concerné :<select id="combo_compteur_evenement" name="id_compteur">
+    {0}
+    </select></p>'''.format(options)
+    return html
