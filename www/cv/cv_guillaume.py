1d7f2d0ba96f0a75208c0e261863296fbe823893
Pastre <guillaume.pastre@gmail.com>
2015-05-26 19:14:31 +0200

    [Rapport] Début du rapport + CV Pastre

--- www/cv/cv_guillaume.py
+++ www/cv/cv_guillaume.py
@@ -5,7 +5,7 @@ def index(error=''):
 
     <head>
         <meta charset="utf-8"/>
-        <title>CV - DZIECIOL Niolas</title>
+        <title>CV - PASTRE Guillaume</title>
         <link rel="stylesheet" href="../cv.css" />
     </head>
 
@@ -13,49 +13,48 @@ def index(error=''):
     <header>
         <div id="retour"><br/><a href="../../page_cv.py/index" title="asasourdoire" >Retour au site</a></div>
         <div id="title">
-            <h1>DZIECIOL Nicolas<br/><br/>Etudiant à l'ENAC<br/>Première année du cycle ingénieur</h1><br/>
+            <h1>PASTRE Guillaume<br/><br/>Etudiant à l'ENAC<br/>Première année du cycle ingénieur</h1><br/>
 
      </div>
         <div id="photo">
             <img width="100px" src="../polo.png" alt="Nicolas">
         </div>
-            <p>101 rue Saint Roch<br/>
-            31400 TOULOUSE<br/>
-            <span class="mail" >nicolas.dzieciol@gmail.com</span>
+            <p>Chambre 339 Résidence Védrines, 7 Avenue Edouard Belin<br/>
+            31055 TOULOUSE<br/>
+            <span class="mail" >guillaume.pastre@alumni.enac.fr</span>
             </p>
     </header>
 
     <section>
     <h1>Formation</h1>
     <ul>
-            <li><strong>2013-2015 :</strong> Première année d'école d'ingénieur à l'ENAC à Toulouse.</li>
+            <li><strong>2014-2015 :</strong> Première année d'école d'ingénieur à l'ENAC à Toulouse.</li>
         </ul>
         <ul>
-            <li><strong>2011-2013 :</strong> Première et deuxième année en CPGE, lycée scientifique et technologique Gustave Eiffel (Dijon), option Technologie et Sciences de l'Ingénieurs.</li>
+            <li><strong>2012-2014 :</strong>Classe préparatoire aux grandes écoles, lycée Joffre, Montpellier filière MP</li>
         </ul>
 
 
         <h1>Diplômes</h1>
         <ul>
-            <li><strong>juin 2011 :</strong> Baccalauréat technologique (STI génie Électrotechnique).</li>
+            <li><strong> 2012 :</strong> Baccalauréat scientifique mention Bien, option « Sciences de la Vie et de la Terre » au lycée Emmanuel d'Alzon à Nîmes </li>
+            <li><strong> 2011 :</strong> Brevet d'inititation à l'aéronautique </li>
         </ul>
 
         <h1>Expériences Professionnelles</h1>
 
         <ul>
-            <li><strong>2013 (2 mois) : </strong> SEGER : Emploi saisonnier, monteur électricien à Montbard.</li>
+            <li><strong>2009 : </strong> Découverte de l'entreprise Adrea Mutuelle à l'agence de Nîmes.</li>
         </ul>
-        <ul>
-            <li><strong>2012 (2 mois) :</strong> SNCF : Emploi saisonnier, ASCT (Agent du service commercial trains) à Dijon.</li>
-        </ul>
+
 
 
         <h1>Compétences</h1>
 
         <ul>
-        <li><strong>Langues :</strong>  anglais (intermédiaire)</li>
-        <li><strong>Software :</strong> Usage régulier de Windows et Ubuntu. Suite Microsoft Office, Autodesk Inventor, Adobe Photoshop.</li>
-        <li><strong>Programmation :</strong> Langage C, Python</li>
+        <li><strong>Langues :</strong>  anglais courant</li>
+        <li><strong>Software :</strong> Usage des suites bureautiques Windows et Ubuntu</li>
+        <li><strong>Programmation :</strong> Python</li>
         <li><strong>Divers :</strong> Permis B</li>
         </ul>
 
@@ -63,7 +62,7 @@ def index(error=''):
 
         <h1>Loisirs et Associations</h1>
         <ul>
-            <li><strong>Divers :</strong> Informatique, photographie.</li>
+            <li>Voyages à l'étranger : Irlande, Ecosse, Angleterre, Etats-Unis</li>
         </ul>
     </section>
     </body>
