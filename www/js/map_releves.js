882bdd78536896bc46f539a18fad7e47481a13ac
Fabien-B <fabien.bonneval@gmail.com>
2015-05-26 21:38:58 +0200

    [releves] popup on mapp

--- www/js/map_releves.js
+++ www/js/map_releves.js
@@ -6,7 +6,7 @@ get_network(map_releves,onEach_upReleve);
 function onEach_upReleve(feature, layer) {
     if (feature.properties && feature.properties["altitude"]) {
     	var texte = String("Altitude: " + feature.properties["altitude"] + "m");
-		layer.on('click', function(e) {oncompteurClick(this,feature.properties["id"]);});
+		layer.bindPopup(texte).on('click', function(e) {oncompteurClick(this,feature.properties["id"]);});
     }
 }
 
@@ -15,5 +15,4 @@ function onEach_upReleve(feature, layer) {
 function oncompteurClick(compteur,id_compteur) {
 	change_combo_box(id_compteur);
 	update_index_deb_releve(-1,id_compteur);
-	compteur.setIcon(selectedIcon);
 }
