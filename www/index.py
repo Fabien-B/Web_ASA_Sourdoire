b061c4bc91567a7a7b3540128270bda01fc22f5d
didzoskill <nicolas.dzieciol@gmail.com>
2015-06-03 14:55:51 +0200

    W3C modif index

--- www/index.py
+++ www/index.py
@@ -14,33 +14,33 @@ def corps_accueil():
             <div class="sixteen columns main-content">
                 <div class="sixteen columns">
                     <div class="tagline">
-                        <p>Bienvenue sur le site de l'<strong>ASA Sourdoire</strong>, le site de gestion de l'irrigation par internet.<br>
+                        <p>Bienvenue sur le site de l'<strong>ASA Sourdoire</strong>, le site de gestion de l'irrigation par internet.<br />
                         Simple. Minimal. Easy-to-use.</p>
                     </div>
-                    <hr>
+                    <hr />
                     <p style ="font-size: large; text-align: justify;">(Re)Découvrez l'ASA Sourdoire désormais connecté ! Vous pouvez désormais rentrer vos relevés de chez vous par internet,
                      votre consommation est alors mise à jour en direct, et les dialogues avec le gestionnaire sont désormais plus simples.</p>
                      <br />
-                    <hr>
-                    <section class="container">
+                    <hr />
+                    <section><div class="container">
 
         <div class="one-third column" style ="text-align: center;" >
-        <img src="../images/logo_conso.png" height="60em" />
+        <img src="../images/logo_conso.png" height="60px" alt="logo_conso" />
             <h3> Consommation</h3>
             <p style ="text-align: justify;">Vous pouvez consulter votre consommation quand vous voulez, suivre l'évolution de celle ci et garder un suivi personnalisé de vos bornes et parcelles.</p>
         </div>
         <div class="one-third column" style ="text-align: center;">
-        <img src="../images/logo_releve.png" height="60em" />
+        <img src="../images/logo_releve.png" height="60px" alt="logo_releve" />
             <h3> Finie la galère des relevés </h3>
             <p style ="text-align: justify;">Grâce à ce nouveau système vous pouvez entrer vous même un relevé, plus besoin d'intermédiaire !</p>
         </div>
         <div class="one-third column" style ="text-align: center;">
-        <img src="../images/signal.png" height="60em" />
+        <img src="../images/signal.png" height="60px" alt="signal" />
             <h3> Signalez les problèmes</h3>
             <p style ="text-align: justify;">Maintenant vous pouvez signalez quand il y a un soucis sur une des bornes, un relevé, consulter l'état du réseau et bien plus encore...</p>
         </div>
 
-        </section>
+        </div></section>
 
                      <br/>
                      <fieldset>
@@ -59,7 +59,7 @@ def traiterFormulaireConnexion(choix, login='',password=''):
 # fonctions AFFICHAGE
 def afficherAccueil():
     ret='''
-        <body style="padding:0px; margin:0px; font-family:Arial, Verdana;background-color:#fff;">
+        <div style="padding:0px; margin:0px; font-family:Arial, Verdana;background-color:#fff;">
 
     <script type="text/javascript" src="../js/jssor.js"></script>
     <script type="text/javascript" src="../js/jssor.slider.js"></script>
@@ -71,7 +71,7 @@ def afficherAccueil():
     <div id="slider1_container" style="position: relative; margin: 0 auto;
         top: 0px; left: 0px; width: 1300px; height: 300px; overflow: hidden; max-height:300px; margin-top:-30px;">
         <!-- Loading Screen -->
-        <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+        <div id="loading" style="position: absolute; top: 0px; left: 0px;">
             <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block;
                 top: 0px; left: 0px; width: 100%; height: 100%;">
             </div>
@@ -153,73 +153,6 @@ def afficherAccueil():
         </div>
 
         <!--#region Bullet Navigator Skin Begin -->
-
-        <style>
-            /* jssor slider bullet navigator skin 21 css */
-            /*
-            .jssorb21 div           (normal)
-            .jssorb21 div:hover     (normal mouseover)
-            .jssorb21 .av           (active)
-            .jssorb21 .av:hover     (active mouseover)
-            .jssorb21 .dn           (mousedown)
-            */
-            .jssorb21 {
-                position: absolute;
-            }
-            .jssorb21 div, .jssorb21 div:hover, .jssorb21 .av {
-                position: absolute;
-                /* size of bullet elment */
-                width: 19px;
-                height: 19px;
-                text-align: center;
-                line-height: 19px;
-                color: white;
-                font-size: 12px;
-                background: url(../images/accueil/picture_slider/b21.png) no-repeat;
-                overflow: hidden;
-                cursor: pointer;
-            }
-            .jssorb21 div { background-position: -5px -5px; }
-            .jssorb21 div:hover, .jssorb21 .av:hover { background-position: -35px -5px; }
-            .jssorb21 .av { background-position: -65px -5px; }
-            .jssorb21 .dn, .jssorb21 .dn:hover { background-position: -95px -5px; }
-        </style>
-        <!-- bullet navigator container -->
-        <div u="navigator" class="jssorb21" style="bottom: 26px; right: 6px;">
-            <!-- bullet navigator item prototype -->
-            <div u="prototype"></div>
-        </div>
-        <!--#endregion Bullet Navigator Skin End -->
-
-        <!--#region Arrow Navigator Skin Begin -->
-
-        <style>
-            /* jssor slider arrow navigator skin 21 css */
-            /*
-            .jssora21l                  (normal)
-            .jssora21r                  (normal)
-            .jssora21l:hover            (normal mouseover)
-            .jssora21r:hover            (normal mouseover)
-            .jssora21l.jssora21ldn      (mousedown)
-            .jssora21r.jssora21rdn      (mousedown)
-            */
-            .jssora21l, .jssora21r {
-                display: block;
-                position: absolute;
-                /* size of arrow element */
-                width: 55px;
-                height: 55px;
-                cursor: pointer;
-                background: url(../images/accueil/picture_slider/a21.png) center center no-repeat;
-                overflow: hidden;
-            }
-            .jssora21l { background-position: -3px -33px; }
-            .jssora21r { background-position: -63px -33px; }
-            .jssora21l:hover { background-position: -123px -33px; }
-            .jssora21r:hover { background-position: -183px -33px; }
-            .jssora21l.jssora21ldn { background-position: -243px -33px; }
-            .jssora21r.jssora21rdn { background-position: -303px -33px; }
-        </style>
         <!-- Arrow Left -->
         <span u="arrowleft" class="jssora21l" style="top: 123px; left: 8px;">
         </span>
@@ -230,7 +163,7 @@ def afficherAccueil():
 
     </div>
     <!-- Jssor Slider End -->
-</body>
+</div>
         '''
     return ret
 
